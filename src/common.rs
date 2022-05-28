2e002ce63a58a37a9051e5350accf7cb490cec69
WhiredPlanck <whiredplanck@outlook.com>
2022-05-29 00:09:34 +0800

    common: add is_leap_year

--- src/common.rs
+++ src/common.rs
@@ -53,3 +53,12 @@ pub const EARTHLY_BRANCHES: [&str; 12] = [
     "Zi", "Chou", "Yin", "Mou", "Chen", "Si", "Wu", "Wei", "Shen", "You", "Xu",
     "Hai",
 ];
+
+pub fn is_leap_year(year: u32) -> bool {
+    match (year % 4, year % 100, year % 400) {
+        (0, 0, 0) => true,
+        (0, 0, _) => false,
+        (0, _, _) => true,
+        (_, _, _) => false,
+    }
+}
